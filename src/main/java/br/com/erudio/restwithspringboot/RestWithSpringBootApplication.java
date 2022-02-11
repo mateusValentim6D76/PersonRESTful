c86a8f349f8306869517ac5cda6823132f90ca22
mateusValentim6D76 <mateus.valentim19@hotmail.com>
2022-02-10 21:08:26 -0300

    Primeiro commit

---
+++ src/main/java/br/com/erudio/restwithspringboot/RestWithSpringBootApplication.java
@@ -0,0 +1,13 @@
+package br.com.erudio.restwithspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWithSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestWithSpringBootApplication.class, args);
+	}
+
+}
