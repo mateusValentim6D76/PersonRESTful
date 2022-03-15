35b5b61fee559524eeea12c2c142f4b7cafb7b42
mateusValentim6D76 <mateus.valentim19@hotmail.com>
2022-03-14 22:49:25 -0300

    Tratamento de Exceções

---
+++ src/main/java/br/com/erudio/restwithspringboot/exception/ExceptionResponse.java
@@ -0,0 +1,37 @@
+package br.com.erudio.restwithspringboot.exception;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class ExceptionResponse implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private Date timestamp;
+	private String message;
+	private String details;
+	
+	public ExceptionResponse(Date date, String message, String details) {
+
+		this.timestamp = date;
+		this.message = message;
+		this.details = details;
+	}
+
+	public Date getTimestamp() {
+		return timestamp;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public String getDetails() {
+		return details;
+	}
+	
+	
+	
+
+	
+}
