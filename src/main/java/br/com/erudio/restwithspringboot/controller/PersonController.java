742cf7d0491200cd21d9043757e8d7b21239af73
mateusValentim6D76 <mateus.valentim19@hotmail.com>
2022-04-03 10:57:01 -0300

    Retornando lista de objetos

--- src/main/java/br/com/erudio/restwithspringboot/controller/PersonController.java
+++ src/main/java/br/com/erudio/restwithspringboot/controller/PersonController.java
@@ -1,8 +1,6 @@
 package br.com.erudio.restwithspringboot.controller;
 
-import java.util.concurrent.atomic.AtomicLong;
-
-import javax.websocket.server.PathParam;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -11,9 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.erudio.restwithspringboot.converter.NumberConverter;
-import br.com.erudio.restwithspringboot.exception.UnsuportedMathOperationException;
-import br.com.erudio.restwithspringboot.math.SimpleMath;
 import br.com.erudio.restwithspringboot.model.Person;
 import br.com.erudio.restwithspringboot.service.PersonServices;
 
@@ -23,6 +18,12 @@ public class PersonController {
 
 	@Autowired
 	private PersonServices service;
+	
+	@RequestMapping(method = RequestMethod.GET, produces =  MediaType.APPLICATION_JSON_VALUE)
+	public List<Person> findAll(){
+		return service.findAll();
+	}
+	
 	
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	public Person findById(@PathVariable("id") String id){
--- src/main/java/br/com/erudio/restwithspringboot/service/PersonServices.java
+++ src/main/java/br/com/erudio/restwithspringboot/service/PersonServices.java
@@ -1,5 +1,7 @@
 package br.com.erudio.restwithspringboot.service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.stereotype.Service;
@@ -10,7 +12,7 @@ import br.com.erudio.restwithspringboot.model.Person;
 public class PersonServices {
 
 	private final AtomicLong counter = new AtomicLong();
-	
+
 	public Person findById(String id) {
 		Person person = new Person();
 		person.setId(counter.incrementAndGet());
@@ -18,7 +20,27 @@ public class PersonServices {
 		person.setLastName("Valentim");
 		person.setAddress("Osasco - São Paulo");
 		person.setGender("Male");
-		return person ;
+		return person;
 	}
-	
+
+	public List<Person> findAll() {
+		List<Person> persons = new ArrayList<>();
+		for (int i = 0; i < 8; i++) {
+			Person person = mockPerson(i);
+			persons.add(person);
+		}
+
+		return persons;
+	}
+
+	private Person mockPerson(int i) {
+		Person person = new Person();
+		person.setId(counter.incrementAndGet());
+		person.setFirstName("Mateus" + i);
+		person.setLastName("Valentim" + i);
+		person.setAddress("Osasco - São Paulo" + i);
+		person.setGender("Male" + i);
+		return person;
+	}
+
 }
