6fbcf489ddf1d5846a39a191c9ff8af5a741e025
mateusValentim6D76 <mateus.valentim19@hotmail.com>
2022-07-04 22:28:43 -0300

    Ajustando o service e o controller

--- src/main/java/br/com/erudio/restwithspringboot/controller/PersonController.java
+++ src/main/java/br/com/erudio/restwithspringboot/controller/PersonController.java
@@ -3,7 +3,7 @@ package br.com.erudio.restwithspringboot.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,7 +13,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.erudio.restwithspringboot.service.PersonServices;
+import br.com.erudio.restwithspringboot.converter.DozerConverter;
+import br.com.erudio.restwithspringboot.data.model.Person;
+import br.com.erudio.restwithspringboot.repository.PersonRepository;
 import br.com.erudio.restwithspringboot.vo.PersonVO;
 
 @RestController
@@ -21,32 +23,45 @@ import br.com.erudio.restwithspringboot.vo.PersonVO;
 public class PersonController {
 
 	@Autowired
-	private PersonServices service;
+	private PersonRepository repository;
 
 	@PostMapping
 	public PersonVO create(@RequestBody PersonVO person) {
-		return service.create(person);
+		var entity = DozerConverter.parseObject(person, Person.class);
+		var vo = DozerConverter.parseObject(repository.save(entity), PersonVO.class);
+		return vo;
 	}
 
 	@GetMapping
 	public List<PersonVO> findAll() {
-		return service.findAll();
+		return DozerConverter.parseListObject(repository.findAll(), PersonVO.class);
 	}
 
 	@GetMapping("/{id}")
 	public PersonVO findById(@PathVariable("id") Long id) {
-		return service.findById(id);
+		var entity = repository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("No records found for this id " + id));
+		return DozerConverter.parseObject(entity, PersonVO.class);
 	}
 
 	@PutMapping
-	public PersonVO update(@RequestBody PersonVO person) {
-		return service.update(person);
+	public PersonVO update(PersonVO person) {
+		var entity = repository.findById(person.getId())
+				.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID"));
+		entity.setFirstName(person.getFirstName());
+		entity.setLastName(person.getLastName());
+		entity.setAddress(person.getAddress());
+		entity.setGender(person.getGender());
+		
+		var vo = DozerConverter.parseObject(repository.save(entity), PersonVO.class);
+		return vo;
 	}
 	
 	@DeleteMapping("/{id}")
-	public ResponseEntity<?> delete(@PathVariable("id") Long id) {
-		service.delete(id);
-		return ResponseEntity.ok().build();
+	public void delete(@PathVariable("id") Long id) {
+		Person entity = repository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("No records found for this id " + id));
+		repository.delete(entity);
 	}
 
 }
