909424b8ab43edb90e64162b20bf49cb0a0367ca
Mateus Valentim de Oliveira <mateus.valentim19@hotmail.com>
2023-02-17 01:17:43 -0300

    Add anotation @EnableAutoConfiguration, @ComponentScan

--- src/main/java/br/com/erudio/restwithspringboot/RestWithSpringBootApplication.java
+++ src/main/java/br/com/erudio/restwithspringboot/RestWithSpringBootApplication.java
@@ -1,9 +1,13 @@
 package br.com.erudio.restwithspringboot;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan
 public class RestWithSpringBootApplication {
 
 	public static void main(String[] args) {
