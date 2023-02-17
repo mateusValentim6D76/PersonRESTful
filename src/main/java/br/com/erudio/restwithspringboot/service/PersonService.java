3db75db760942386ef7388012daff307deb1b801
Mateus Valentim de Oliveira <mateus.valentim19@hotmail.com>
2023-02-16 23:55:55 -0300

    Implementando o Hateoas no PersonService.findById

--- src/main/java/br/com/erudio/restwithspringboot/service/PersonService.java
+++ src/main/java/br/com/erudio/restwithspringboot/service/PersonService.java
@@ -4,7 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import static  org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static  org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
+import com.github.dozermapper.core.DozerBeanMapper;
+
+import br.com.erudio.restwithspringboot.controller.PersonController;
 import br.com.erudio.restwithspringboot.converter.DozerConverter;
 import br.com.erudio.restwithspringboot.converter.custom.PersonConverter;
 import br.com.erudio.restwithspringboot.data.model.Person;
@@ -21,40 +26,42 @@ public class PersonService {
 
 	@Autowired
 	private PersonConverter converter;
-	
+
 	public PersonVO create(PersonVO person) {
-		var entity = DozerConverter.parseObject(person, Person.class); 
-		var vo = DozerConverter.parseObject(personRepository.save(entity), PersonVO.class); 
+		var entity = DozerConverter.parseObject(person, Person.class);
+		var vo = DozerConverter.parseObject(personRepository.save(entity), PersonVO.class);
 		return vo;
-		
+
 	}
-	
+
 	public PersonVOV2 createV2(PersonVOV2 person) {
-		var entity = converter.convertVoToEntity(person); 
-		var vo = converter.convertEntityToVo(personRepository.save(entity)); 
+		var entity = converter.convertVoToEntity(person);
+		var vo = converter.convertEntityToVo(personRepository.save(entity));
 		return vo;
-		
+
 	}
-	
+
 	public List<PersonVO> findAll() {
 		return DozerConverter.parseListObject(personRepository.findAll(), PersonVO.class);
 	}
 
 	public PersonVO findById(Long id) {
-		var entity =  personRepository.findById(id)
+		var entity = personRepository.findById(id)
 				.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID " + id));
-		return DozerConverter.parseObject(entity, PersonVO.class);
+		PersonVO vo = DozerConverter.parseObject(entity, PersonVO.class);
+		vo.add(linkTo(methodOn(PersonController.class).findById(id)).withSelfRel());
+		return vo;
 	}
 
 	public PersonVO update(PersonVO person) {
-		var entity = personRepository.findById(person.getId())
+		var entity = personRepository.findById(person.getKey())
 				.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID"));
-		
+
 		entity.setFirstName(person.getFirstName());
 		entity.setLastName(person.getLastName());
 		entity.setAddress(person.getAddress());
 		entity.setGender(person.getGender());
-		
+
 		var vo = DozerConverter.parseObject(personRepository.save(entity), PersonVO.class);
 		return vo;
 	}
--- src/main/java/br/com/erudio/restwithspringboot/vo/v1/PersonVO.java
+++ src/main/java/br/com/erudio/restwithspringboot/vo/v1/PersonVO.java
@@ -3,16 +3,19 @@ package br.com.erudio.restwithspringboot.vo.v1;
 import java.io.Serializable;
 import java.util.Objects;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.springframework.hateoas.RepresentationModel;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.github.dozermapper.core.Mapping;
 
-@JsonPropertyOrder({ "id", "first_name", "last_name", "gender", "address" })
-public class PersonVO implements Serializable {
+@JsonPropertyOrder({ "key", "first_name", "last_name", "gender", "address" })
+public class PersonVO extends RepresentationModel<PersonVO> implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
-	private Long id;
+	@Mapping("id")
+	private Long key;
 
 	@JsonProperty("first_name")
 	private String firstName;
@@ -21,19 +24,19 @@ public class PersonVO implements Serializable {
 	private String lastName;
 
 	private String address;
-	
+
 	private String gender;
 
 	public PersonVO() {
 
 	}
 
-	public Long getId() {
-		return id;
+	public Long getKey() {
+		return key;
 	}
 
-	public void setId(Long id) {
-		this.id = id;
+	public void setKey(Long key) {
+		this.key = key;
 	}
 
 	public String getFirstName() {
@@ -70,20 +73,23 @@ public class PersonVO implements Serializable {
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(address, firstName, gender, id, lastName);
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + Objects.hash(address, firstName, gender, key, lastName);
+		return result;
 	}
 
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
 			return true;
-		if (obj == null)
+		if (!super.equals(obj))
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
 		PersonVO other = (PersonVO) obj;
 		return Objects.equals(address, other.address) && Objects.equals(firstName, other.firstName)
-				&& Objects.equals(gender, other.gender) && Objects.equals(id, other.id)
+				&& Objects.equals(gender, other.gender) && Objects.equals(key, other.key)
 				&& Objects.equals(lastName, other.lastName);
 	}
 
