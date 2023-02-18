61f333212b8bd1dcf31591417371fa3d4c1d321d
Mateus Valentim de Oliveira <mateus.valentim19@hotmail.com>
2023-02-18 20:48:05 -0300

    Customizando a documentacao Swagger - FindAll, FindById

--- src/main/java/br/com/erudio/restwithspringboot/controller/PersonController.java
+++ src/main/java/br/com/erudio/restwithspringboot/controller/PersonController.java
@@ -15,33 +15,67 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.erudio.restwithspringboot.service.PersonService;
 import br.com.erudio.restwithspringboot.vo.v1.PersonVO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
 @RequestMapping("api/person/v1")
+@Tag(name = "People", description = "Endpoint for Managing People")
 public class PersonController {
+
+	private static final String APPLICATION_JSON = "application/json";
 
 	@Autowired
 	private PersonService service;
 
 	@GetMapping(produces = { "application/json", "application/xml", "application/x-yaml" })
+	@Operation(summary = "Finds all People", description = "Finds all People", tags = { "People" }, responses = {
+			@ApiResponse(description = "Success", responseCode = "200", content = {
+					@Content(mediaType = APPLICATION_JSON, array = @ArraySchema(schema = @Schema(implementation = PersonVO.class))
+					) 
+					}),
+			@ApiResponse(description = "Success", responseCode = "200", content = @Content),
+			@ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+			@ApiResponse(description = "Bad Request", responseCode = "404", content = @Content),
+			@ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)})
 	public List<PersonVO> findAll() {
 		return service.findAll();
 
 	}
 
 	@GetMapping(value = "/{id}", produces = { "application/json", "application/xml", "application/x-yaml" })
+	@Operation(summary = "Find a People", description = "Find a People", tags = { "People" }, responses = {
+			@ApiResponse(description = "Success", responseCode = "200", content = 
+					  @Content(schema =  @Schema(implementation = PersonVO.class))),
+			@ApiResponse(description = "Success", responseCode = "200", content = @Content),
+			@ApiResponse(description = "No Content", responseCode = "204", content = @Content),
+			@ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+			@ApiResponse(description = "Bad Request", responseCode = "404", content = @Content),
+			@ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)})
 	public PersonVO findById(@PathVariable("id") Long id) {
 		return service.findById(id);
 	}
 
-	@PostMapping(produces = { "application/json", "application/xml","application/x-yaml" }, consumes = { "application/json", "application/xml",
-			"application/x-yaml"})
+	@PostMapping(produces = { "application/json", "application/xml", "application/x-yaml" }, consumes = {
+			"application/json", "application/xml", "application/x-yaml" })
+	@Operation(summary = "Find a People", description = "Find a People", tags = { "People" }, responses = {
+			@ApiResponse(description = "Success", responseCode = "200", content = 
+					  @Content(schema =  @Schema(implementation = PersonVO.class))),
+			@ApiResponse(description = "Success", responseCode = "200", content = @Content),
+			@ApiResponse(description = "No Content", responseCode = "204", content = @Content),
+			@ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+			@ApiResponse(description = "Bad Request", responseCode = "404", content = @Content),
+			@ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)})
 	public PersonVO create(@RequestBody PersonVO person) {
 		return service.create(person);
 	}
 
-	@PutMapping(produces = { "application/json", "application/xml", "application/x-yaml" }, consumes = { "application/json", "application/xml",
-			"application/x-yaml" })
+	@PutMapping(produces = { "application/json", "application/xml", "application/x-yaml" }, consumes = {
+			"application/json", "application/xml", "application/x-yaml" })
 	public PersonVO update(PersonVO person) {
 		return service.update(person);
 	}
