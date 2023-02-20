5693f5ffd57aeca33357ec98e7228fefebc460c8
Mateus Valentim de Oliveira <mateus.valentim19@hotmail.com>
2023-02-20 01:53:56 -0300

    Adicionando o CORS no Person Controller

--- src/main/java/br/com/erudio/restwithspringboot/controller/PersonController.java
+++ src/main/java/br/com/erudio/restwithspringboot/controller/PersonController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -44,6 +45,7 @@ public class PersonController {
 	}
 
 	@GetMapping(value = "/{id}", produces = { "application/json", "application/xml", "application/x-yaml" })
+	@CrossOrigin(origins = "http://localhost:8080")
 	@Operation(summary = "Finds a People", description = "Finds a People", tags = { "People" }, responses = {
 			@ApiResponse(description = "Success", responseCode = "200", content = @Content(schema = @Schema(implementation = PersonVO.class))),
 			@ApiResponse(description = "No Content", responseCode = "204", content = @Content),
@@ -57,6 +59,7 @@ public class PersonController {
 
 	@PostMapping(produces = { "application/json", "application/xml", "application/x-yaml" }, consumes = {
 			"application/json", "application/xml", "application/x-yaml" })
+	@CrossOrigin(origins = {"http://localhost:8080", "https://mvalentimlearning.com.br"})
 	@Operation(summary = "Adds a new Person", description = "Adds a new Person by passing in a JSON, XML or YML representation of the person !", tags = {
 			"People" }, responses = {
 					@ApiResponse(description = "Success", responseCode = "200", content = @Content(schema = @Schema(implementation = PersonVO.class))),
