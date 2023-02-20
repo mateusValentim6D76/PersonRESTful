c15a7ae913fe53df7e8cdff8803399ccd2624735
Mateus Valentim de Oliveira <mateus.valentim19@hotmail.com>
2023-02-20 19:28:02 -0300

    Mapeando a classe Permission

---
+++ src/main/java/br/com/erudio/restwithspringboot/data/model/Permission.java
@@ -0,0 +1,58 @@
+package br.com.erudio.restwithspringboot.data.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.springframework.security.core.GrantedAuthority;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "permission")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Permission implements GrantedAuthority, Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Column
+	private String description;
+
+	@Override
+	public String getAuthority() {
+		return this.description;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(description, id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Permission other = (Permission) obj;
+		return Objects.equals(description, other.description) && Objects.equals(id, other.id);
+	}
+
+}
