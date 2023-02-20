caa41f510324350610af33d3acd88db00caab0fe
Mateus Valentim de Oliveira <mateus.valentim19@hotmail.com>
2023-02-20 02:01:40 -0300

    Implementando o Cors Origin de forma globa, implementando metodo > addCorsMappings e application.prop

--- src/main/java/br/com/erudio/restwithspringboot/config/WebConfig.java
+++ src/main/java/br/com/erudio/restwithspringboot/config/WebConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import br.com.erudio.restwithspringboot.serialization.converter.YamlJackson2HttpMessageConverter;
@@ -15,8 +16,20 @@ public class WebConfig implements WebMvcConfigurer {
 	
 	private static final MediaType MEDIA_TYPE_YAML = MediaType.valueOf("application/x-yaml");
 	
+	private String crossOriginPatterns = "${cors.originPatterns:default}";
+	
 	public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
 		converters.add(new YamlJackson2HttpMessageConverter());
+	}
+	
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		var allowedOrigins = crossOriginPatterns.split(",");
+		registry.addMapping("/**")
+		//.allowedMethods("GET", "POST", "PUT", "DELETE")
+		.allowedMethods("*")
+		.allowedOrigins(allowedOrigins)
+	.allowCredentials(true);
 	}
 //	@Override
 //	public void configureContentNegotiation(ContentNegotiationConfigurer config) {
