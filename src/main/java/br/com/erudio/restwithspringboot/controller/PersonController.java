4905ff18bb251e7a12a077497eb5b8e6a4be24c7
Mateus Valentim de Oliveira <mateus.valentim19@hotmail.com>
2023-03-01 19:31:33 -0300

    criando o método findPersonsByName e disablePerson

--- src/main/java/br/com/erudio/restwithspringboot/controller/PersonController.java
+++ src/main/java/br/com/erudio/restwithspringboot/controller/PersonController.java
@@ -1,7 +1,6 @@
 package br.com.erudio.restwithspringboot.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
--- src/main/java/br/com/erudio/restwithspringboot/repository/PersonRepository.java
+++ src/main/java/br/com/erudio/restwithspringboot/repository/PersonRepository.java
@@ -1,9 +1,21 @@
 package br.com.erudio.restwithspringboot.repository;
 
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import br.com.erudio.restwithspringboot.data.model.Person;
 
 public interface PersonRepository extends JpaRepository<Person, Long> {
 
+	@Modifying
+	@Query("UPDATE Person p SET p.enable = false WHERE p.id =:id")
+	void disablePerson(@Param("id") Long id);
+	
+
+	@Modifying
+	@Query("SELECT p FROM Person p WHERE p.firstName LIKE LOWER (CONCAT ('%',:firstName,'%'))")
+	Page<Person> findPersonsByName(@Param("firstName") String firstName);
 }
