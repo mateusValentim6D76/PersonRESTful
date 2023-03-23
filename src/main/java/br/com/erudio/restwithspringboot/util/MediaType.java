d466cf13c9258951bbb81d0635afab53c8fc2345
Mateus Valentim de Oliveira <mateus.valentim19@hotmail.com>
2023-03-22 23:23:49 -0300

    Implement MediaType.util

---
+++ src/main/java/br/com/erudio/restwithspringboot/util/MediaType.java
@@ -0,0 +1,7 @@
+package br.com.erudio.restwithspringboot.util;
+
+public class MediaType {
+
+	public static final org.springframework.http.MediaType APPLICATION_YAML = org.springframework.http.MediaType
+			.valueOf("application/x-yaml");
+}
