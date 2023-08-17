014fbbf06566bf564edb0b8c7374a6ab8c3e5ee7
mateusValentim <mateus.valentim19@hotmail.com>
2023-08-17 20:55:52 -0300

    Add TokenVO and AccountCredentialsVO in package integrationtests

---
+++ src/test/java/br/com/erudio/restwithspringboot/integrationtests/vo/AccountCredentialsVO.java
@@ -0,0 +1,39 @@
+package br.com.erudio.restwithspringboot.integrationtests.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class AccountCredentialsVO implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private String username;
+	private String password;
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(password, username);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		AccountCredentialsVO other = (AccountCredentialsVO) obj;
+		return Objects.equals(password, other.password) && Objects.equals(username, other.username);
+	}
+
+}
---
+++ src/test/java/br/com/erudio/restwithspringboot/integrationtests/vo/TokenVO.java
@@ -0,0 +1,51 @@
+package br.com.erudio.restwithspringboot.integrationtests.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class TokenVO implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private String username;
+
+	private Boolean authenticated;
+
+	private Date created;
+
+	private Date expirated;
+
+	private String accessToken;
+
+	private String refreshToken;
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(accessToken, authenticated, created, expirated, refreshToken, username);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		TokenVO other = (TokenVO) obj;
+		return Objects.equals(accessToken, other.accessToken) && Objects.equals(authenticated, other.authenticated)
+				&& Objects.equals(created, other.created) && Objects.equals(expirated, other.expirated)
+				&& Objects.equals(refreshToken, other.refreshToken) && Objects.equals(username, other.username);
+	}
+
+}
